a315d767983b2ee5d87166962f4cdea370a94f0e
amal ibrahim <aml_naruto@yahoo.com>
2022-12-10 20:46:47 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,30 @@
+
+import React from "react";
+import Header from "./header/header";
+// import Loader from "./loader/loader.js";
+import Sidebar from "./sidebar/sidebar.js";
+
+
+
+function App() {
+
+
+   return (
+   <div className="App">
+
+
+    <Header/>
+
+        {/* <Sidebar/> */}
+
+        {/* <Loader/> */}
+
+  
+
+
+  </div>
+
+  );
+}
+
+export default App;
